dde5efe368aeb46d66c7e9d4dc8e8b4ea982c2ea
Johnnpham7496 <johnpham7496@gmail.com>
2023-06-08 16:25:48 -0500

    created while loop to ensure user is entering an integer

--- password_generator.py
+++ password_generator.py
@@ -1,6 +1,13 @@
 import random
-# prompting user to input length of a password in an integer format
-pass_len = int(input("Enter the length of your password here: "))
+# loop to make sure user is entering an integer and not a string character
+# this generator does not exceed 74 characters
+while True: 
+    try:
+        pass_len = int(input("Enter the length of your password here: "))
+        break
+    except ValueError:
+        print('Invalid input. Please enter a integer')
+
 s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
 # this will take the users int input and randomly select characters and numbers from the 's' variable to generate a random password
 p = "".join(random.sample(s,pass_len))
