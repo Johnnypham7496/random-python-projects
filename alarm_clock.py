8947b5b4bc6560a482f365f7a7fec6ab149c7ccd
Johnnpham7496 <johnpham7496@gmail.com>
2023-06-07 23:46:02 -0500

    added email_slicer.py file and updated other files with notes

--- alarm_clock.py
+++ alarm_clock.py
@@ -3,7 +3,7 @@ import winsound
 
 
 alarm_time = input("Enter the time of alarm to be set: HH:MM:SS\n")
-
+# the square brackets represent the placement of the numbers within alarm_time
 alarm_hour = alarm_time[0:2]
 alarm_minute = alarm_time[3:5]
 alarm_second = alarm_time[6:8]
@@ -13,11 +13,12 @@ print("Setting up alarm..")
 
 while True:
     now = datetime.now()
+    # strftime is used to format a datetime aka 'string format time'
     current_hour = now.strftime("%I")
     current_min = now.strftime("%M")
     current_sec = now.strftime("%S")
     current_period = now.strftime("%p")
-
+    # multiple if statements to make sure the datetime matches with the alarm time that is entered 
     if alarm_period==current_period:
         if alarm_hour == current_hour:
             if alarm_minute == current_min:
