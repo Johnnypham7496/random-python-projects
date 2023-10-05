0bc9cd0ed9ad1056bf41188ebda4f162af72af02
Johnnpham7496 <johnpham7496@gmail.com>
2023-10-04 22:09:22 -0500

    started file for shopping cart

---
+++ miscellaneous/shopping_cart.py
@@ -0,0 +1,13 @@
+# shopping cart program
+
+
+foods = []
+prices = []
+total = 0
+
+
+while True:
+    food = input('Enter a food to buy (q to quit): ')
+    if food.lower() == 'q':
+        break
+    
