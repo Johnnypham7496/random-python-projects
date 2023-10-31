e794c2be40b52ef7b900b135d041f12d2a68a01a
Johnnpham7496 <johnpham7496@gmail.com>
2023-10-30 22:11:41 -0500

    added phone_camera app code and updated requirements.txt

---
+++ advanced/phone_camera.py
@@ -0,0 +1,14 @@
+# this will require the IP webcam app from the playstore/appstore
+# once the app is downloaded you will need the IP address given from the app and place it in our code below
+import cv2
+import numpy as np
+url = "Your IP Address/video"
+cp = cv2.VideoCapture(url)
+while(True):
+    camera, frame = cp.read()
+    if frame is not None:
+        cv2.imshow("Frame", frame)
+    q = cv2.waitKey(1)
+    if q==ord("q"):
+        break
+cv2.destroyAllWindows()
